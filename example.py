b0e01c8dfefc9ad003b96bc6fc0da3d60eaacc90
Kenan Ibrović <kenan.ibrovic94@gmail.com>
2018-03-26 23:55:33 +0200

    reconfigured with waypoints

--- example.py
+++ example.py
@@ -1,44 +1,48 @@
 import googlemaps
 import re
 from pprint import pprint
-from datetime import datetime
+from datetime import datetime, timedelta
 
 gmaps = googlemaps.Client(key='AIzaSyDzEneQdlBYzQ5ubhoLprvwU9BFzxI5Eug')
 
-def get_distance(origins, destinations):
+def get_distance(directions_response):
     """get distance between two or more places"""
-    distance = gmaps.distance_matrix(
-        origins=origins,
-        destinations=destinations
-    )
-    return distance['rows'][0]['elements'][0]
+    distance = [i['distance']['value'] for i in directions_response]
+    return sum(distance)/1000
 
-def get_directions(origin, destination):
+def get_duration(directions_response):
+    """get time traveling between locations"""
+    duration = [i['duration']['value'] for i in directions_response]
+    return timedelta(seconds=sum(duration))
+
+
+def get_directions(origin, destination, waypoints=None):
     """Get directions to destination from origin"""
     directions = gmaps.directions(
-        origin=origin,
-        destination=destination,
+        origin,
+        destination,
+        waypoints=waypoints,
         mode="driving",
         departure_time=datetime.now()
     )
-    return directions[0]['legs'][0]['steps']
+    return directions[0]['legs']
 
 
 def check_for_countries(directions_response):
     """Extract all countries entered"""
     print("checking for countries")
     countries = []
-    coords = [i['end_location'] for i in directions_response]
+    coords = [i['end_location'] for j in range(len(directions_response)) for i in directions_response[j]['steps']]
 
     for i in range(len(coords)):
         try:
             print("checking %d geolocation" % i, end='\r')
             geocode = gmaps.reverse_geocode((str(coords[i]['lat']) + ',' + str(coords[i]['lng'])), result_type='country')
             countries.append(geocode[0]['address_components'][0]['long_name'])
-        except IndentationError as e:
+        except IndentationError:
             print("IndentationError:")
             continue
-        except IndexError as e:
+        except IndexError:
             print("IndexError")
             continue
     countries = list(set(countries))
@@ -46,17 +50,18 @@ def check_for_countries(directions_response):
 
 class RouteInfo:
     """Class to recieve directions object"""
-    def __init__(self, origin, destination):
+    def __init__(self, origin, destination, waypoints):
         self.origin = origin
         self.destination = destination
-        self.distance = get_distance(self.origin, self.destination)['distance']['text']
-        self.duration = get_distance(self.origin, self.destination)['duration']['text']
-        self.directions = get_directions(self.origin, self.destination)
+        self.waypoints = waypoints
+        self.directions = get_directions(self.origin, self.destination, waypoints)
+        self.distance = get_distance(self.directions)
+        self.duration = get_duration(self.directions)
         self.countries = check_for_countries(self.directions)
         
     
-def make_a_class(class_name, origin, destination):
-    class_name = RouteInfo(origin, destination)
+def make_a_class(class_name, origin, destination, waypoints):
+    class_name = RouteInfo(origin, destination, waypoints)
     return class_name
 
 
@@ -64,8 +69,14 @@ origin = input("Enter starting point: ").strip(".,- ")
 print(origin)
 destination = input("Enter delivering point: ").strip(".,- ")
 print(destination)
+waypoints = []
+stop = 'n'
+while stop!='y':
+    waypoints.append(input("Waypoints: "))
+    stop = input("Stop (y/[n])")
+print('|'.join(waypoints))
 new_name = origin + "_" + destination
-new_name = make_a_class(new_name, origin, destination)
+new_name = make_a_class(new_name, origin, destination, waypoints)
 
 stop = 'n'
 while stop != 'y':
@@ -88,6 +99,22 @@ while stop != 'y':
         '6': new_name.countries
     }
     val = info_dir[info]
-    pprint(val)
+    print(val)
 
     stop = input("Do you want to stop? (y): ")
+
+
+
+#Used for testing
+#szb = RouteInfo("sarajevo", "beograd", "zagreb")
+#pprint(szb.duration)
+#szb = get_directions("sarajevo", "beograd", "zagreb")
+'''szb = gmaps.directions(
+    "sarajevo",
+    "beograd",
+    waypoints="zagreb",
+    mode="driving"
+)'''
+#pprint(szb)
+#print(check_for_countries(szb))
+#print(get_duration(szb))
